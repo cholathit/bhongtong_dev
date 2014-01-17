ef4bd172c67900a6bc7fef60a33041e34d430832
Cholathit Khueankaew <kcholathit@gmail.com>
2014-01-17 15:44:13 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BhongtongDev::Application.config.secret_key_base = 'daf32363969a9d64851ae4ec73fc7487ef2fd81a45adfd0eaf22e6f07e76c24938508e7d27e7a121dfd41d3f7c00d46c2aa61b0efdd1f2e8e37ce8ccb6087271'
